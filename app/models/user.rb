c4f569ef736cea13c276ab688a4adc4a6272b86c
Akshay Borade <akshay.cs.borade@gmail.com>
2025-02-13 08:29:02 +0530

    Init commit with Bootstrap, Devise setup

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum role: { developer: 0, manager: 1, admin: 2 }       
+end
